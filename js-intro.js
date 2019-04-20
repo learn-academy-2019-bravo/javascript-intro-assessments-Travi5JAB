b999384621471a9c5f5b1260aa97cf8fa7a54f12
Travis Baker <travis841235@gmail.com>
2019-04-20 16:42:22 -0700

    assignment submission

--- js-intro.js
+++ js-intro.js
@@ -5,18 +5,18 @@
 var mantra = "Be the dev";
 
 // 1. Write the code that returns 'B' from mantra.
-
+console.log(mantra.charAt(0))
 // 2. Write the code that determines if there is a 'x' in mantra.
-
+console.log(mantra.includes("x"));
 // 3. Write the code that determines if there is a 'v' in mantra.
-
+console.log(mantra.includes("v"));
 // Stretch: Write the code that returns the position of 'v' in mantra.
-
+console.log(mantra.indexOf("v"));
 
 // Consider the following statement:
 
-var codingIsEasy
-var imBored
+var codingIsEasy = true
+var imBored = true
 
 if (codingIsEasy && imBored) {
     console.log("I need more!")
@@ -28,8 +28,8 @@ if (codingIsEasy && imBored) {
 
 // Consider the following statement:
 
-var imLost
-var imFrustrated
+var imLost = false
+var imFrustrated = false
 
 if (imLost || imFrustrated) {
     console.log('Break time!')
@@ -40,17 +40,45 @@ if (imLost || imFrustrated) {
 // 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
 
 // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
+for (let i = 0; i< mantra.length; i++){
 
+  console.log(mantra[i])
+}
+console.log("");
 // Stretch: Using a while loop.
-
+let ind = 0
+while( ind< mantra.length ){
+  console.log(mantra[ind])
+  ind++
+}
 // Super Stretch: Using forEach().
 
 // Consider the following variable:
-
+console.log("");
 var message = "thisisateststring"
-
+var number = 55
+var boolean = true
 // 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+var vowel = ["a","e","i","o","u"]
+var newStr = message.split()
+function remVowel(string){
+    var newArr = []
+    if (typeof string != "string"){
+      return "ERROR: This is a "+typeof string+ " not a string"
+    }else{
+        for(i=0; i<string.length; i++){
+            if(string[i]!= "a" && string[i]!= "e" && string[i]!= "i" && string[i]!= "o" && string[i]!= "u"){
+                newArr.push(string[i])
+                var x = newArr.join("")
+              }
 
+        }return x
+      }
+}
+console.log(remVowel(message));
+console.log(remVowel(number));
+console.log(remVowel(boolean));
+console.log("");
 // Stretch: Update your function to throw an exception if the argument is not a string.
 
 // Super Duper Stretch: Update your function to throw a TypeError if the argument is not a string.
@@ -60,3 +88,19 @@ var message = "thisisateststring"
 var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
 
 // 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+
+function cats(arr){
+//make for loop and use obj.animal
+var newArr =[]
+var newArr1 =[]
+  for(let i = 0; i< arr.length; i++){
+     if(arr[i].animal === "cat" ){
+       newArr.push(arr[i])
+
+
+     }
+  }return newArr
+
+}
+
+console.log(cats(toonimals));
--- js-intro.md
+++ js-intro.md
@@ -3,13 +3,15 @@
 Answer these questions, then follow the instructions to submit your first assessment!
 
 #### 1. What is a function? Why would you use one?
-
+A function is a set of statements that performs a task or calculates a value. A function must be defined within the scope in which it is used.
 #### 2. What is a method?
-
+A method contains a function definition. They are used to perform an action on an object.
 #### 3. What are some examples of javascript string methods?
-
+concat() is a method that takes 2 strings and concatenates them(makes them one string)
+charAt() is a method that returns a character in a string that resides at the desired index
 #### 4. Explain the difference between accessor and mutator methods. Why would you use one over the other?
-
+accessors allow access to data and mutators allow to change data
 #### 5. Explain the differences between console.log() and return. Why would you use one over the other?
-
+console.log() prints the result to the terminal when the program is run. return is used to return a value much like the console.log() however return will not display the value to the terminal unless used with console.log()
 #### 6. What is one thing we did in class this week that helped you learn the most?  
+creating functions in js that are used in html in order to create an interactive experience for users. (maic 8 ball, pluralizer, etc..)
